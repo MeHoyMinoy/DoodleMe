a69facb57d3dc8d0bb2419df17739d0c5557472c
apio9090 <apio9009@gmail.com>
2018-09-26 22:05:47 -0400

    Added Stuff

---
+++ app/src/main/java/com/example/apio9009/doodleme/DrawingActviity.java
@@ -0,0 +1,14 @@
+package com.example.apio9009.doodleme;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DrawingActviity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawing);
+    }
+
+}
